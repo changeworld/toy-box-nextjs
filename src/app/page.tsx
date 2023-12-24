55527bd14e44518ca45c26cefdccde73a6bb8343
Takashi Takebayashi <changesworlds@gmail.com>
2023-12-24 18:14:50 +0900

    Modify page
    - path /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+'use client'
+
 export default function Home() {
+  const signIn = async () => {
+    try {
+      const response = await fetch('/api/auth/microsoft/sign-in')
+      if (!response.ok) {
+        throw new Error(`message: HTTP error, status: ${response.status}`)
+      }
+      const data = await response.json()
+      window.location.href = data.redirect_url
+    } catch (error) {
+      console.error('An error occurred while retrieving data sign in:', error)
+    }
+  }
+
   return (
     <main>
+      <button onClick={() => signIn()}>サインイン</button>
     </main>
   )
 }
